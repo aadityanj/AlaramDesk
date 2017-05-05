be3ebabade43a7561bae50610e3ea9e5b96dbdf6
Adithya <adithya@hasotech.com>
2017-05-06 01:17:56 +0530

    seperated images and tones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -57,7 +57,7 @@ export class AppComponent {
         if(flag == 1 ) {
            if(this.flagforsound){
                var audio = new Audio();
-               audio.src = "app/home/warning.mp3";
+               audio.src = "app/tones/warning.mp3";
                audio.load();
                audio.play();
                setTimeout(function() {
